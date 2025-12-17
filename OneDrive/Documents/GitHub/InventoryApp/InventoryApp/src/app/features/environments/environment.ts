f1c655aff5d702056bf7d3de2982428681732e8a
Dan Andrei Espiritu <danandreiespiritu@gmail.com>
2025-12-17 09:13:12 +0800

    InventoryApp

--- OneDrive/Documents/GitHub/InventoryApp/InventoryApp/src/app/features/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5044/api',
-    authPaths: {
-        login: '/auth/Login',
-        register: '/auth/Register'
-    }
-}
--- OneDrive/Documents/GitHub/InventoryApp/InventoryApp/src/app/features/model/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './AuthModels';
-export * from './Users';
-export * from './RefreshToken';
-export * from './Brands';
-export * from './Inventy';
